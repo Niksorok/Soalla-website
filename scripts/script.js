bb658a05add4cb53ad67bcfa40197ed5fcc97844
Niksor <Salfordyoutube@ya.ru>
2023-12-19 20:29:10 +0300

    Добавил новое платье

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,28 @@
 const Dresses = document.querySelector(".dresses");
 const Model = document.querySelector(".model");
 
-const Button = document.querySelector(".open-model");
+const OpenSonya = document.querySelector("#open_Sonya");
+const OpenLada = document.querySelector("#open_Lada");
 
-Button.addEventListener('click', () => {
+function DressView(dress){
     Dresses.classList.add("dresses_close");
     Model.classList.add("model_open");
-});
+
+    document.querySelector("#neck-0").src = `./image/${dress}_neck-0.png`;
+    document.querySelector("#neck-1").src = `./image/${dress}_neck-1.png`;
+    document.querySelector("#neck-2").src = `./image/${dress}_neck-2.png`;
+
+    document.querySelector("#chest_belly-0").src = `./image/${dress}_chest_belly-0.png`;
+    document.querySelector("#chest_belly-1").src = `./image/${dress}_chest_belly-1.png`;
+    document.querySelector("#chest_belly-2").src = `./image/${dress}_chest_belly-2.png`;
+
+    document.querySelector("#legs-0").src = `./image/${dress}_legs-0.png`;
+    document.querySelector("#legs-1").src = `./image/${dress}_legs-1.png`;
+    document.querySelector("#legs-2").src = `./image/${dress}_legs-2.png`;
+}
+
+OpenSonya.addEventListener('click', () => { DressView('Sonya')});
+OpenLada.addEventListener('click', () => { DressView('Lada')});
 
 new Swiper('.swiper-container', {
     spaceBetween: 0,
